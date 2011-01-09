ba94123671dee41b21f050b1bd22e75f9945ff8b
MF <feleck@archy.chello.pl>
2011-01-09 19:50:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Printers::Application.config.secret_token = 'f8ead5e5d8e8a07fc832693feaeee4500ef3cc095aeb7a051a0bc8a54e962ae5a9389c5fc1aaf72360e5c54cbce4987bbe99a3d2b98f71f2c211c893ec111fdb'
